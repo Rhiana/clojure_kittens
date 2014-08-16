5d4815e26964a7cbefbf1406e4d5626f0c379115
Rhiana Heath <rhiana@heath.cc>
2014-08-16 17:08:22 +1000

    testing guessing game

--- src/kittens/handler.clj
+++ src/kittens/handler.clj
@@ -3,6 +3,9 @@
             [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(def last-result
+  (atom 0))
+
 (defroutes app-routes
   (GET "/" [] "
     <form action='/calculate'>
@@ -14,11 +17,36 @@
         <input type='number' id='second_number' name='second_number' step='any'>
         <input type='submit' value='Add!'>
       </fieldset>
+    </form>
+    <form action='/guessing'>
+      <fieldset>
+        <legend>What number am I thinking of?</legend>
+        <label for='guess_number'>Guess a Number between 1 and 100</label>
+        <input type='number' id='guess_number' name='guess_number'>
+        <input type='submit' value='Guess!'>
+      </fieldset>
     </form>")
+
   (GET "/calculate" [first_number second_number]
-    (str first_number " + " second_number " = "
-      (+  (Integer/parseInt first_number)
-          (Integer/parseInt second_number))))
+    (let  [result (+ (Integer/parseInt first_number)
+          (Integer/parseInt second_number))
+        last-num (deref last-result)]
+      (reset! last-result result)
+      (str "Last: " last-num
+           "<br>New: " first_number " + " second_number " = " result
+           "<br><a href='/'>Back</a>")))
+
+  (GET "/guessing" [guess_number]
+      ( let [winning_number 27]
+        (if (= guess_number winning_number) (def message "You win!")
+          (if (> guess_number winning_number) (def message "Too big try again") (def message "Too small try again"))
+        )
+        (str "Your number was " guess_number
+        "<br> The winning number was " winning_number
+        "Your number was " message
+        "<br><a href='/'>Back</a>"))
+    )
+
 
     (route/resources "/")
     (route/not-found "Not Found"))
